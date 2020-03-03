982ddff00691225fb7a76cbe7a9de0e9ee23b991
ishvar99 <ishanvarshney99@gmail.com>
2020-03-03 18:55:05 +0530

    added other modules

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const path =require('path')
+var router=express.Router();
+
+router.get('/add-product',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+})
+router.post('/add-product',(req,res,next)=>{  
+    console.log(req.body);
+    res.redirect('/');
+})
+module.exports=router;
